e8cb7e48b3441baef432391036d90457cd99b374
RiccardoCenzon <riccardo.cenzon06@gmail.com>
2022-10-15 06:36:57 +0200

    .

---
+++ 11_numero_magico.py
@@ -0,0 +1,10 @@
+numero_magico = 7
+i=1
+while i <= 10:
+        i=i+1
+        numero_utente=int(input("Inserisci il numero magico:"))
+        if(numero_utente == numero_magico): 
+            print("Complimenti, hai indovinato!")
+            break
+        else:
+            print("Game over!")
