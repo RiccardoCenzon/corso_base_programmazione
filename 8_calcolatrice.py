563df6079bce23315e06764ed92fa61a5420d9ae
RiccardoCenzon <riccardo.cenzon06@gmail.com>
2022-10-26 20:28:32 +0200

    .

---
+++ 8_calcolatrice.py
@@ -0,0 +1,18 @@
+numero_1=int(input("Inserisci il primo numero:"))
+numero_2=int(input("Inserisci il secondo numero:"))
+numero_operazione=int(input("Inserisci il numero dell'operazione:"))
+if(numero_operazione==1):
+    output=numero_1+numero_2
+if(numero_operazione==2):
+    output=numero_1-numero_2
+if(numero_operazione==3):
+    output=numero_1*numero_2
+if(numero_operazione==4):
+    output=numero_1/numero_2 
+    if(numero_2==0):
+        output=("Non puoi dividere per zero!")
+if(numero_operazione<1):
+    output=numero_1+numero_2
+if(numero_operazione>4):
+    output=numero_1+numero_2
+print(output) 
