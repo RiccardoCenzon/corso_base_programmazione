83ce38d4ca2a317aedf7a6c2d5eab8fea188551b
RiccardoCenzon <riccardo.cenzon06@gmail.com>
2022-11-02 21:22:08 +0100

    .

---
+++ 3_stampa_nomi.py
@@ -0,0 +1,12 @@
+lettera=input("Inserisci una lettera:")
+messaggio_da_stampare="Hai scelto in modo errato!"
+if(lettera=="A"):
+    messaggio_da_stampare="Hai scelto Aldo!"
+if(lettera=="B"):
+    messaggio_da_stampare="Hai scelto Bruno!"
+if(lettera=="C"):
+    messaggio_da_stampare="Hai scelto Carlo!"
+if(lettera=="D"):
+    messaggio_da_stampare="Hai scelto Dario!"
+print(messaggio_da_stampare)
+lettera=input("Press any key to exit")
