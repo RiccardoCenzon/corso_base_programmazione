172f1e61207feca6f7ac3f9bd48cffc18b9bc979
RiccardoCenzon <riccardo.cenzon06@gmail.com>
2022-11-02 21:46:55 +0100

    .

--- 11_numero_magico.py
+++ 11_numero_magico.py
@@ -1,12 +1,16 @@
 numero_magico = 7
 i=1
+indovinato=0
 while i <= 10:
         i=i+1
         numero_utente=int(input("Inserisci il numero magico:"))
         if(numero_utente == numero_magico): 
             print("Complimenti, hai indovinato!")
+            indovinato=1   
             break
         else:
-            print("Game over!")
+            print("Hai sbagliato numero!")
+if(indovinato==0):
+    print("Game over!")
 A=input("Press any key to exit.")
 
