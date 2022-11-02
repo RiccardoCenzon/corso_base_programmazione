da06df5864f6c995d0352119bd79911167be3345
RiccardoCenzon <riccardo.cenzon06@gmail.com>
2022-11-02 21:14:56 +0100

    .

---
+++ 2_lato_quadrato.py
@@ -0,0 +1,6 @@
+lato_quadrato=int(input("Inserisci la misura del lato:"))
+area_quadrato=lato_quadrato*lato_quadrato
+perimetro_quadrato=lato_quadrato*4
+print("L'area misura:" ,area_quadrato)
+print("Il perimetro misura:" ,perimetro_quadrato)
+a=input("Press any key to exit")
